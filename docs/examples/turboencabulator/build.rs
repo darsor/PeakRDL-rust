8c103a474f3e9a0a0a3b0bfff9a25f9abf6062cd
Dana Sorensen <dana.r.sorensen@gmail.com>
2026-03-11 20:51:02 -0600

    fix example turboencabulator docs

--- docs/examples/turboencabulator/build.rs
+++ docs/examples/turboencabulator/build.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use peakrdl_rust_build::Generator;
 
 fn main() -> Result<()> {
-    unsafe { std::env::set_var("PEAKRDL_RUST_BINARY", "../../../scripts/uv_wrapper.sh") };
+    unsafe { std::env::set_var("PEAKRDL_RUST_BINARY", "../../../scripts/uv_peakrdl_rust.sh") };
     Generator::new()
         .rdl_file("../../../src/peakrdl_rust/udps/udps.rdl")
         .rdl_file("../../../tests/rdl_src/turboencabulator.rdl")
